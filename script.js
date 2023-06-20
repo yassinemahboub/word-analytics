b1e0205f314ab68166bfadb6db5ffb803fcf9835
Yassine <132377798+yassinemahboub@users.noreply.github.com>
2023-06-20 19:30:54 +0100

    Created Initial JS Structure

---
+++ script.js
@@ -0,0 +1,7 @@
+const textareaEl = document.querySelector('.textarea');
+
+textareaEl.addEventListener('input', function () {
+    // determine new number 
+    textareaEl.value
+    //set new numbers
+})
